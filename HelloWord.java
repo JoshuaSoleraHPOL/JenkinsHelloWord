b729f18d1a4466a0e0c968d82a627bfd35568fc1
JoshuaSoleraHPOL <113196436+JoshuaSoleraHPOL@users.noreply.github.com>
2022-09-09 11:09:44 -0500

    Update HelloWord.java
    Adding a simple change to watch how work Jenkins with a repo in github

--- HelloWord.java
+++ HelloWord.java
@@ -2,5 +2,6 @@ public class HelloWord{
 	public static void main(String[] args)
 	{
 		System.out.println("----***Hello Word & wellcome to Jenkins Training****----");
+		System.out.println("Add new changes");
 	}
 }
